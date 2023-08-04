c7695f1e29e3a27dc4e5f52d71e858c956873ac4
Rian <rian.azevedo@qintess.com>
2023-08-03 22:51:19 -0300

    Relative Layout

---
+++ app/src/main/java/com/example/relativelayout/LoginRelativeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gastoviagem.R
+
+class LoginRelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_relative)
+    }
+}
---
+++ app/src/main/java/com/example/relativelayout/RelativeLayout.kt
@@ -0,0 +1,12 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gastoviagem.R
+
+class RelativeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
